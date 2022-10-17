42bd30dae52fe496be52d705af0eac46e239e876
ImMega <72713009+ImMega@users.noreply.github.com>
2022-10-17 22:42:08 +0200

    Update randomActivities.js

--- randomActivities.js
+++ randomActivities.js
@@ -17,7 +17,6 @@ module.exports = (client) => {
     setInterval(() => {
         const rngType = Math.floor(Math.random() * a.length);
         const rngName = Math.floor(Math.random() * a[rngType].name.length);
-        console.log(`${a[rngType].type} --- ${a[rngType].name[rngName]}`);
 
         client.user.setActivity({ type: a[rngType].type, name: a[rngType].name[rngName] });
     }, 7000);
