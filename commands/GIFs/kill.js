2010a865cabf204704fceb5540ca0b82cabb27ed
ImMega <72713009+ImMega@users.noreply.github.com>
2022-11-06 02:40:15 +0100

    Update kill.js

--- commands/GIFs/kill.js
+++ commands/GIFs/kill.js
@@ -17,7 +17,7 @@ module.exports = {
             embeds: [
                 new EmbedBuilder()
                 .setColor(message.guild.members.me.displayHexColor)
-                .setTitle(mention == message.guild.members.me.user ? gif.includes("kill9.gif") ? "Agh-" : "#@*$&UDNI#J" : gif.includes("kill9.gif") ? "And whoop, it snapped" : "OH he ded")
+                .setTitle(mention == message.guild.members.me.user ? gif.includes("kill9.gif") ? "Agh-" : "#@*$&UDNI#J" : gif.includes("kill9.gif") ? "And whoop, it snapped" : "OH they ded")
                 .setDescription(`${message.author} got ${mention == message.guild.members.me.user ? "me" : mention} umm yea kinda ded${mention == message.guild.members.me.user ? ". Is this heaven?" : ""}`)
                 .setImage(gif)
                 .setFooter({ text: "Gifs provided by: kawaii.red" })
