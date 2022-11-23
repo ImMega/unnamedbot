94dfd3910d544f05ac21fb4bd6f74935fe7e98be
ImMega <72713009+ImMega@users.noreply.github.com>
2022-11-24 00:34:15 +0100

    Update kill.js

--- commands/GIFs/kill.js
+++ commands/GIFs/kill.js
@@ -1,5 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
 
+const kill = [
+    "OH they ded",
+    "Aw hell nawh, I'm outta here"
+]
+
 module.exports = {
     name: "kill",
     aliases: [],
@@ -13,11 +18,13 @@ module.exports = {
 
         const gif = await require("../../kawaii-api").getGIF("kill");
 
+        const random = Math.floor(Math.random() * kill.length);
+
         message.channel.send({
             embeds: [
                 new EmbedBuilder()
                 .setColor(message.guild.members.me.displayHexColor)
-                .setTitle(mention == message.guild.members.me.user ? gif.includes("kill9.gif") ? "Agh-" : "#@*$&UDNI#J" : gif.includes("kill9.gif") ? "And whoop, it snapped" : "OH they ded")
+                .setTitle(mention == message.guild.members.me.user ? gif.includes("kill9.gif") ? "Agh-" : "#@*$&UDNI#J" : gif.includes("kill9.gif") ? "And whoop, it snapped" : kill[random])
                 .setDescription(`${message.author} got ${mention == message.guild.members.me.user ? "me" : mention} umm yea kinda ded${mention == message.guild.members.me.user ? ". Is this heaven?" : ""}`)
                 .setImage(gif)
                 .setFooter({ text: "Gifs provided by: kawaii.red" })
