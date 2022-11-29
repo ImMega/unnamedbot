9bb871f900ab784e0869f1254f6223139f798704
ImMega <72713009+ImMega@users.noreply.github.com>
2022-11-29 19:41:34 +0100

    Fix counting

--- helpers/counting.js
+++ helpers/counting.js
@@ -1,30 +1,31 @@
 const serverModel = require("../models/serverSchema");
 
 module.exports = async (message) => {
-    let lastMsg = (await message.channel.messages.fetch({ limit: 1, before: message.id })).first().content;
+    const lastMsg = (await message.channel.messages.fetch({ limit: 1, before: message.id })).first();
+    let lastCount = lastMsg.content;
 
-    const lastArgs = lastMsg.split(/ +/);
+    const lastArgs = lastCount.split(/ +/);
 
-    if(lastArgs.length == 1 && isNaN(lastMsg) && message.content == 1) return;
-    if(lastArgs.length == 1 && isNaN(lastMsg) && message.content > 1) {
+    if(lastArgs.length == 1 && isNaN(lastCount) && message.content == 1) return;
+    if(lastArgs.length == 1 && isNaN(lastCount) && message.content > 1) {
         message.delete();
         message.author.createDM().send(`You start with 1 not ${message.content}!!!`);
         return;
     }
 
     if(lastArgs.length > 1 && isNaN(lastArgs.find(a => !isNaN(a)))) return;
-    if(lastArgs.length > 1 && !isNaN(lastArgs.find(a => !isNaN(a)))) lastMsg = lastArgs.find(a => !isNaN(a));
+    if(lastArgs.length > 1 && !isNaN(lastArgs.find(a => !isNaN(a)))) lastCount = lastArgs.find(a => !isNaN(a));
 
-    lastMsg++;
+    lastCount++;
 
     let num = message.content;
 
     const args = message.content.split(/ +/);
 
-    if(args.length > 1) num = await args.find(s => s == lastMsg)
+    if(args.length > 1) num = await args.find(s => s == lastCount)
 
     if(isNaN(num)) return message.delete();
-    if(num != lastMsg) return require("../helpers/wrongCountPunish")(message, lastMsg, num);
+    if(num != lastCount || message.author.id == lastMsg.author.id) return require("../helpers/wrongCountPunish")(message, lastCount, num, message.author.id == lastMsg.author.id);
 
     await serverModel.findOneAndUpdate({
         serverId: message.guild.id
--- helpers/wrongCountPunish.js
+++ helpers/wrongCountPunish.js
@@ -1,9 +1,13 @@
-module.exports = async (message, required, inputed) => {
+module.exports = async (message, required, inputed, same) => {
     message.delete();
+
     const dm = await message.author.createDM(true);
 
     let last = required;
     last--;
 
-    dm.send(`Learn how to count b-baka!!! After **${last}** comes **${required}**, not **${inputed}**!`);
+    const wrongCount = `Learn how to count b-baka!!! After **${last}** comes **${required}**, not **${inputed}**!`;
+    const samePerson = `Oi! Wait for your turn to count!!!`;
+
+    dm.send(same == true ? samePerson : wrongCount);
 }
