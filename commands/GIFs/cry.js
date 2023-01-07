b75618b6a77851937612e4ccf9962d9aa396ce35
ImMega <72713009+ImMega@users.noreply.github.com>
2023-01-07 18:28:12 +0100

    New GIFs

---
+++ commands/GIFs/cry.js
@@ -0,0 +1,56 @@
+const { EmbedBuilder } = require("discord.js");
+
+const name = "cry";
+const desc = "Cry, if you really need to...";
+
+module.exports = {
+    name: name,
+    aliases: [],
+    description: desc,
+    usage: "cry",
+    slash: {
+        name: name,
+        description: desc
+    },
+
+    async interactionInit(interaction) {
+        await interaction.deferReply();
+
+        this.execute(interaction, 1);
+    },
+
+    async msgInit(message, args) {
+        this.execute(message, 0);
+    },
+
+    async execute(message, type) {
+        const gif = await require("../../kawaii-api").getGIF("cry");
+
+        this.reply.send(message, type, {
+            embeds: [
+                new EmbedBuilder()
+                .setColor(message.member.displayHexColor)
+                .setDescription(`${message.member.user} broke down crying... Poor thing...`)
+                .setImage(gif)
+                .setFooter({ text: "GIF provided by: kawaii.red" })
+            ]
+        });
+    },
+
+    reply: {
+        async send(message, type, content) {
+            if(!type) {
+                return message.channel.send(content);
+            } else {
+                return message.editReply(content);
+            }
+        },
+        async reply(message, type, content) {
+            if(!type) {
+                return message.reply(content);
+            } else {
+                return message.editReply(content);
+            }
+        }
+    }
+}
