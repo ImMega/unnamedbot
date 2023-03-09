78e23a8f6a338d45ea17514e26d21362abc163f7
ImMega <72713009+ImMega@users.noreply.github.com>
2023-03-09 18:59:10 +0100

    Update countChannel.js

--- commands/Admins Only/countChannel.js
+++ commands/Admins Only/countChannel.js
@@ -5,10 +5,14 @@ module.exports = {
     aliases: ["countch"],
     description: "Sets a counting channel so the bot can see if you know how to count",
     usage: "countchannel <channel>",
-    async execute(message, args){
+    async msgInit(message, args){
         if(!message.member.permissions.has("ADMINISTRATOR")) return;
 
         const channelId = args[0].replace("<#", "").replace(">", "");
+
+        const channel = await message.guild.channels.cache.get(channelId);
+
+        if(!channel) return message.reply("Channel with that ID does not exist in this server.");
 
         await serverModel.findOneAndUpdate({ serverId: message.guild.id }, { countChId: channelId });
 
